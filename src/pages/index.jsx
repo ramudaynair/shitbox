6923b77a32f28982808493e49f5dc3da02b87a07
Ram Uday <ramudaynair@gmail.com>
2025-06-15 16:22:33 +0530

    commit

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,11 +16,19 @@ const geistMono = Geist_Mono({
 });
 
 export default function Home() {
+  useEffect(async() => {
+    const ax=await axios.post("http://localhost:3001/createdata", {
+      "name": "blakpjohn",
+      "city": "ekm",
+      "age": 19
+    })
+    console.log("Response from server", ax);
+  }, []);
 
-  
+
   return (
     <>
-      
+
     </>
 
   );
